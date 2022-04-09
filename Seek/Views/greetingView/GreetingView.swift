c5ee5d3baa8b6a5dfbc999b1814d320c14b8d439
Armando Taveras <armandogtaveras@gmail.com>
2022-04-09 00:05:59 -0400

    Began working on thestartup page

---
+++ Seek/Views/greetingView/GreetingView.swift
@@ -0,0 +1,20 @@
+//
+//  GreetingView.swift
+//  Seek
+//
+//  Created by Armando Taveras on 4/8/22.
+//
+
+import SwiftUI
+
+struct GreetingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct GreetingView_Previews: PreviewProvider {
+    static var previews: some View {
+        GreetingView()
+    }
+}
