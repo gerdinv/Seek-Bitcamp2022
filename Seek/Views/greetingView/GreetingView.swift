f691baaf869b516fc39ebb86e9002a476ad67195
Armando Taveras <armandogtaveras@gmail.com>
2022-04-09 00:07:33 -0400

    startup page update

--- Seek/Views/greetingView/GreetingView.swift
+++ Seek/Views/greetingView/GreetingView.swift
@@ -9,7 +9,13 @@ import SwiftUI
 
 struct GreetingView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Image("couchGuy")
+                       .resizable()
+                       .scaledToFill()
+                       .edgesIgnoringSafeArea(.all)
+                       
+        }
     }
 }
 
