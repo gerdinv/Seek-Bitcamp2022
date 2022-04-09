f4d322070fd1fee6bb588f8dd66957262ef6a6c7
Gerdin Ventura Croussett <gerdinventuraedu@gmail.com>
2022-04-08 21:21:21 -0400

    initial commit

---
+++ Seek/SeekApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeekApp.swift
+//  Seek
+//
+//  Created by Gerdin Ventura on 4/8/22.
+//
+
+import SwiftUI
+
+@main
+struct SeekApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
